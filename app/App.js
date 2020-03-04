b7af98287eb08909caf70432c0b9071464e57a87
daseul.kim <da91love@gmail.com>
2020-03-04 21:33:40 +0900

    fin

--- app/App.js
+++ app/App.js
@@ -2,20 +2,67 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View, ActivityIndicator, Image, StatusBar } from 'react-native';
 import Weather from './Weather';
 
+const API_KEY = '222a6b0f26a9bc1a6ad4f991e02ad143';
+const KELVIN_STATIC = 273.15;
+
 export default class App extends Component{
   state = {
-    isLoaded: true
+    isLoaded: false,
+    error: null,
+    city: null,
+    tempMax: null,
+    tempMin: null,
+    desc: null,
+    weather: null
   };
 
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+
+        const {latitude, longitude} = position.coords;
+        this._getWeather(latitude, longitude);
+
+        this.setState({
+          isLoaded: true
+        });
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      });
+  }
+
+  _getWeather = (lati, long) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(jsonRes => {
+          const {name: city, main:{temp_max, temp_min}, weather:[{ description: desc, main:weather }]} = jsonRes;
+          console.log(city, temp_max, temp_min, desc, weather);
+          console.log(temp_max - KELVIN_STATIC);
+          console.log(temp_min - KELVIN_STATIC);
+          this.setState({
+            city: city,
+            tempMax: (temp_max - KELVIN_STATIC).toFixed(1),
+            tempMin: (temp_min - KELVIN_STATIC).toFixed(1),
+            desc: desc,
+            weather: weather
+          })
+        })
+  }
+
   render() {
-    const {isLoaded} = this.state;
+    const {isLoaded, error, city, tempMax, tempMin, desc, weather} = this.state;
 
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-        {isLoaded? <Weather/> : (
+        {isLoaded? <Weather tempMax={tempMax} tempMin={tempMin} city={city} desc={desc} weather={weather}/> : (
             <View style={styles.loading}>
               <Text style={styles.loadingText}>Getting weather</Text>
+              { error ? <Text style={styles.errorText}>{error}</Text>: null}
             </View>
         )}
       </View>
@@ -27,6 +74,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
+  },
+  errorText: {
+    color: "red",
+    marginBottom: 40
   },
   loading: {
     flex: 1,
--- app/Weather.js
+++ app/Weather.js
@@ -2,26 +2,79 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from '@expo/vector-icons';
+import PropTypes from 'prop-types';
+
+const WEATHER_CASE = {
+  Rain: {
+    color: ['#00c6fb', '#005bea'],
+    title: '비오는 날입니다',
+    subtitle: '아침에는 차분한 재즈로 시작해보세요',
+    icon: 'ios-rainy'
+  },
+  Clear: {
+    color: ['#fef253', '#ff7300'],
+    title: '오랜만에 맑은 날씨네요?',
+    subtitle: '커튼을 겉고 힘차세 시작해볼까요!',
+    icon: 'ios-sunny'
+  },
+  ThunderStorm: {
+    color: ['#00ecbc', '#007adf'],
+    title: '번개가 칩니다',
+    subtitle: '낙뢰에 주의하세요',
+    icon: 'ios-thunderstorm'
+  },
+  Clouds: {
+    color: ['#d7d2cc', '#304352'],
+    title: '흐린날 입니다',
+    subtitle: '차분하게 책읽기 좋은 날이죠',
+    icon: 'ios-cloudy'
+  },
+  Snow: {
+    color: ['#7de2fc', '#b9b6e5'],
+    title: '눈오는 날입니다',
+    subtitle: '눈은 항상 설레요',
+    icon: 'ios-snow'
+  },
+  Drizzle: {
+    color: ['#b9f7fe', '#66a6ff'],
+    title: '진눈깨비가 내립니다',
+    subtitle: '게이같은 날시네요',
+    icon: 'ios-rainy-outline'
+  },
+};
 
 export default class Weather extends Component {
   render() {
     return (
-        <LinearGradient colors={['#00c6fb', '#005bea']} style={styles.container}>
+        <LinearGradient colors={WEATHER_CASE[this.props.weather].color} style={styles.container}>
 
           <View style={styles.upper}>
-            <Ionicons color='white' size={100} name='ios-rainy'/>
-            <Text style={styles.temperature}>36℃</Text>
+            <Ionicons color='white' size={100} name={WEATHER_CASE[this.props.weather].icon}/>
+            <Text style={styles.city}>{this.props.city}</Text>
+          </View>
+
+          <View style={styles.middle}>
+            <Text style={styles.temperature}>Max {this.props.tempMax}℃</Text>
+            <Text style={styles.temperature}>Min {this.props.tempMin}℃</Text>
           </View>
 
           <View style={styles.lower}>
-            <Text style={styles.title}>WEATHER TITLE</Text>
-            <Text style={styles.subtitle}>FOR MORE INFO</Text>
+            <Text style={styles.title}>{WEATHER_CASE[this.props.weather].title}</Text>
+            <Text style={styles.subtitle}>{WEATHER_CASE[this.props.weather].subtitle}</Text>
           </View>
 
         </LinearGradient>
     );
   }
 }
+
+Weather.propTypes = {
+  city: PropTypes.string.isRequired,
+  tempMax: PropTypes.string.isRequired,
+  tempMin: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  weather: PropTypes.string.isRequired
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -32,9 +85,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
+  city: {
+    fontSize: 40,
+    color: 'white'
+  },
   temperature: {
     fontSize: 32,
     color: 'white'
+  },
+  middle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   lower: {
     flex: 1,
